9e0ff48dc0d87a8c281badd4ac199904c6b55878
Alex Virrueta <alex.virrueta@airbnb.com>
2018-07-18 14:13:10 -0700

    Fix typos in models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Questoin(models.Model):
+class Question(models.Model):
     question_tact = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    question = models.foreignKey(Question, on_delete=models.CASCADE)
-    choice_text = model.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
