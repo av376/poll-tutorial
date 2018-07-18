93fa787044a9b4ae3a561e408cbf5823c85acd65
Alex Virrueta <alex.virrueta@airbnb.com>
2018-07-18 13:26:57 -0700

    Add Question and Choice models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Questoin(models.Model):
+    question_tact = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.foreignKey(Question, on_delete=models.CASCADE)
+    choice_text = model.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
